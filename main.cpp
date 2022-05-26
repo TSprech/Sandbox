0d878956dd8793b96ba76fd54ac64c4740194f3c
TSprech <trey.sprecher@gmail.com>
2022-05-26 11:21:22 -0400

    CTests Set in CMake File

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
+#include "gtest/gtest.h"
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+int main(int ac, char* av[]) {
+  testing::InitGoogleTest(&ac, av);
+  return RUN_ALL_TESTS();
 }
---
+++ test/SimpleTest.cpp
@@ -0,0 +1,9 @@
+//
+// Created by TSprech on 5/26/2022.
+//
+
+#include "gtest/gtest.h"
+
+TEST(Simple, Equality) {
+  EXPECT_EQ(2, 2);
+}
